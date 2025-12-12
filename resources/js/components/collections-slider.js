470c9a6b0a553ff64772bb75f6d7ec98e52f3737
Max <max@wearedigitalcake.com>
2025-12-12 12:57:26 +0000

    collections slider fin

---
+++ resources/js/components/collections-slider.js
@@ -0,0 +1,23 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation, Mousewheel } from 'swiper/modules';
+
+window.init_collections_slider = function(section_id) {
+    const swiper = new Swiper('.swiper-' + section_id, {
+        modules: [Navigation, Mousewheel],
+        spaceBetween: 16,
+        slidesPerView: 'auto',
+        init: false,
+        mousewheel: {
+            forceToAxis: true,
+            sensitivity: 1,
+            releaseOnEdges: false
+        },
+        navigation: {
+            nextEl: '.swiper-button-next-' + section_id,
+            prevEl: '.swiper-button-prev-' + section_id,
+        },
+    });
+    swiper.init();
+};
