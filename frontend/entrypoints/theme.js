05962ff3875d68db4634c93d19f2326cf3863e86
Max <max@wearedigitalcake.com>
2026-01-07 11:04:30 +0000

    mobile menu and header

--- frontend/entrypoints/theme.js
+++ frontend/entrypoints/theme.js
@@ -5,6 +5,7 @@ import '../../resources/js/global/header.js';
 import '../../resources/js/global/drawer-search.js';
 import '../../resources/js/global/predictive-search.js';
 import '../../resources/js/global/drawers.js';
+import '../../resources/js/global/menu-mobile.js';
 import '../../resources/js/global/cart-add.js';
 import '../../resources/js/global/recently-viewed-products.js';
 import '../../resources/js/global/sort-by-handlers.js';
---
+++ resources/js/global/menu-mobile.js
@@ -0,0 +1,25 @@
+const init_menu_mobile = () => {
+	const megaMenuLinks = document.querySelectorAll('.menu-mobile__link--has-children');
+	if (megaMenuLinks)
+		for (const link of megaMenuLinks)
+			link.addEventListener('click', e => {
+				e.preventDefault();
+				const parent = link.parentNode;
+				parent.classList.add('active');
+			});
+	const megaMenuBackButtons = document.querySelectorAll('.button--back');
+	if (megaMenuBackButtons)
+		for (const button of megaMenuBackButtons)
+			button.addEventListener('click', e => {
+				e.preventDefault();
+				if (megaMenuLinks) for (const link of megaMenuLinks) link.parentNode.classList.remove('active');
+			});
+	const megaMenuChildTitles = document.querySelectorAll('.menu-child__title');
+	if (megaMenuChildTitles)
+		for (const title of megaMenuChildTitles)
+			title.addEventListener('click', e => {
+				e.preventDefault();
+				title.parentNode.classList.toggle('open');
+			});
+};
+document.addEventListener('DOMContentLoaded', init_menu_mobile);
