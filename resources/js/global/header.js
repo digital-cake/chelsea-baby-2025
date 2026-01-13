bbe9794cf259f5170856e8d0e3bc51917a8d78df
Max <max@wearedigitalcake.com>
2026-01-13 15:00:41 +0000

    header update

--- resources/js/global/header.js
+++ resources/js/global/header.js
@@ -1,15 +1,17 @@
 const handle_header_scroll = () => {
-    if (window.innerWidth <= 1024) return;
     const header = document.querySelector('.section-header');
+    const announcementBar = document.querySelector('.section-announcement-bar');
     if (!header) return;
+
+    header.style.top = (announcementBar ? announcementBar.offsetHeight : 0) + 'px';
 
     let lastScrollTop = 0;
     let ticking = false;
-    let direction = 'down';
     const scrollUpThreshold = 34;
-    const directionThreshold = 34;
+    const directionThreshold = 0;
 
     const updateHeader = () => {
+        header.style.top = (announcementBar ? announcementBar.offsetHeight : 0) + 'px';
         const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
         if (Math.abs(scrollTop - lastScrollTop) < directionThreshold) {
@@ -18,12 +20,12 @@ const handle_header_scroll = () => {
         }
 
         if (scrollTop > lastScrollTop && scrollTop > directionThreshold) {
-            direction = 'down';
             header.classList.add('scrolling');
+            announcementBar.classList.add('scrolling');
             header.classList.remove('scrolling-up');
         } else if (scrollTop < lastScrollTop) {
-            direction = 'up';
             header.classList.remove('scrolling');
+            announcementBar.classList.remove('scrolling');
 
             if (scrollTop > scrollUpThreshold) {
                 header.classList.add('scrolling-up');
