4cdc9d4698b603a43b0df350e0240421dc9b6a8c
Max <max@wearedigitalcake.com>
2026-01-28 17:09:10 +0000

    ugc amends

--- resources/js/components/ugc-slider.js
+++ resources/js/components/ugc-slider.js
@@ -1,15 +1,21 @@
 import Swiper from 'swiper';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import { Pagination, Mousewheel } from 'swiper/modules';
+import { Pagination, Mousewheel, Autoplay } from 'swiper/modules';
 
 window.init_ugc_slider = function(swiperUgcData) {
+    console.log('swiperUgcData', swiperUgcData);
     const swiper = new Swiper('.swiper-' + swiperUgcData.section_id, {
-        modules: [Pagination, Mousewheel],
-        slidesPerView: "auto",
+        modules: [Pagination, Mousewheel, Autoplay],
+        slidesPerView: 1.2,
         spaceBetween: 22,
         centeredSlides: true,
         initialSlide: 2,
+        loop: swiperUgcData.size >= 10,
+        autoplay: swiperUgcData.autoplay && swiperUgcData.size >= 10 ? {
+            delay: parseInt(swiperUgcData.delay) * 1000,
+            disableOnInteraction: false
+        } : false,
         pagination: swiperUgcData.enable_pagination && swiperUgcData.size > 1 ? {
             el: '.swiper-pagination-' + swiperUgcData.section_id,
             clickable: true,
@@ -21,7 +27,8 @@ window.init_ugc_slider = function(swiperUgcData) {
         breakpoints: {
             650: {
                 spaceBetween: 6,
-            },
+                slidesPerView: "auto",
+            }
         }
     });
     swiper.init();
