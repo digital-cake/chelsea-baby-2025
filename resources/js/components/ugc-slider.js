465613b5e2d7a7af62df5e6ff7fc2471cae035cf
Max <max@wearedigitalcake.com>
2026-01-28 16:55:22 +0000

    ugc rebuild

--- resources/js/components/ugc-slider.js
+++ resources/js/components/ugc-slider.js
@@ -6,33 +6,22 @@ import { Pagination, Mousewheel } from 'swiper/modules';
 window.init_ugc_slider = function(swiperUgcData) {
     const swiper = new Swiper('.swiper-' + swiperUgcData.section_id, {
         modules: [Pagination, Mousewheel],
-        slidesPerView: 1.3,
-        centeredSlides: false,
-        watchSlidesProgress: true,
-        loop: false,
-        init: false,
-        spaceBetween: 23,
+        slidesPerView: "auto",
+        spaceBetween: 22,
+        centeredSlides: true,
+        initialSlide: 2,
         pagination: swiperUgcData.enable_pagination && swiperUgcData.size > 1 ? {
             el: '.swiper-pagination-' + swiperUgcData.section_id,
             clickable: true,
         } : false,
         mousewheel: {
+            invert: false,
             forceToAxis: true,
-            sensitivity: 1
         },
         breakpoints: {
-            480: {
-                slidesPerView: 3,
+            650: {
                 spaceBetween: 6,
-                centeredSlides: true,
-                initialSlide: 2,
             },
-            1024: {
-                slidesPerView: 5,
-                spaceBetween: 6,
-                centeredSlides: true,
-                initialSlide: 2,
-            }
         }
     });
     swiper.init();
