d6e36962eb7974b6ae3586a761da072d02d4a90e
Tom Hayes <tom.hayes@cake.agency>
2026-03-24 12:04:07 +0000

    remove duplicate title and description tags

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   plugins: [
     cleanup(),
     shopify({
-      tunnel: 'https://maxdev.ngrok.app:5173',
+      tunnel: true,
       //tunnel: process.env.TUNNEL_URL, // https://maxdev.ngrok.app:5173 npm run
       snippetFile: 'vite.liquid',
       additionalEntrypoints: [
